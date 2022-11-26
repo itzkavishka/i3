74089594862fde483a34ac96bcc227210cf62675
Kavishka Dahanayaka <82071300+iamkavishka@users.noreply.github.com>
2022-11-26 23:18:07 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,4 +1,3 @@
 sudo apt install i3
-cd ~ 
-sudo nano .config/i3/config
 echo -e "Paste this command in config file\nfor_window [class="^.*"] border pixel 0"
+echo -e "Config file path- \n cd ~ && cd .config/i3/ \n Use text editor to edit the config file(As root btw) "
