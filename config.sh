0b4efa7fbfa19754073bece913d2c7d21adef422
Kavishka Dahanayaka <82071300+xkavishka@users.noreply.github.com>
2023-11-10 14:37:53 +0530

    Delete config.sh

--- config.sh
+++
@@ -1,3 +0,0 @@
-sudo apt install i3
-echo -e "Paste this command in config file\nfor_window [class="^.*"] border pixel 0"
-echo -e "Config file path- \n cd ~ && cd .config/i3/ \n Use text editor to edit the config file(As root btw) "
